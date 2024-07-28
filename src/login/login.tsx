ec477e2a15d3a956a4a0c762dffaa3db9d36a0cc
Bugra Isin <bugraisin@gmail.com>
2024-07-28 20:05:39 +0300

    loginasdsa

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import './LoginForm.css'
-import { TextField } from '@mui/material';
-import logo from './components/assets/logo-color.png'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import './login.css'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    navigate('/home');
-  };
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
