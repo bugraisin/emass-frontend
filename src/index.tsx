64de92616698dba538ed6adc608bb60f23ff2d70
Bugra Isin <bugraisin@gmail.com>
2024-07-28 15:04:43 +0300

    login ve home arasındaki bağlantı sağlandı

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Login from './login/login';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-root.render(
-    <Login />
-);
-
